0f1dbab3990a5c710eab1159b9196b73f60f1e8f
nocarryr <matt@nomadic-recording.com>
2016-08-24 13:52:19 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "python-dispatch",
-    version = "v0.0.2",
+    version = "v0.0.3",
     author = "Matthew Reid",
     author_email = "matt@nomadic-recording.com",
     description = "Lightweight Event Handling",
