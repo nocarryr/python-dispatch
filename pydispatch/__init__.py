30935d91c3ef56c96da6e3ee922e6d95ad6b66e0
nocarryr <matt@nomadic-recording.com>
2017-01-02 18:36:36 -0600

    Add __version__ attribute to package for use in doc build

--- pydispatch/__init__.py
+++ pydispatch/__init__.py
@@ -1,2 +1,9 @@
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.require('python-dispatch')[0].version
+except: # pragma: no cover
+    __version__ = 'unknown'
+
 from pydispatch.dispatch import Dispatcher
 from pydispatch.properties import Property
