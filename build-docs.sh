31c68686280c6b9f03cf4ea1990ecc729584c093
nocarryr <matt@nomadic-recording.com>
2018-07-11 16:27:42 -0500

    Use cli arg from aa12b02a to enable venv package installs

--- build-docs.sh
+++ build-docs.sh
@@ -7,4 +7,4 @@ REPO_DOC_ROOT="doc/source"
 LOCAL_BUILD_DIR="build/html/"
 SPHINX_OPTS="-D templates_path=$PWD/source/_templates"
 
-sphinx-versioning -l $LOCAL_CONF build $REPO_DOC_ROOT $LOCAL_BUILD_DIR -- $SPHINX_OPTS
+sphinx-versioning -l $LOCAL_CONF --install-versions build $REPO_DOC_ROOT $LOCAL_BUILD_DIR -- $SPHINX_OPTS
