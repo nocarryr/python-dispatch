7ff6ba0308504a4e1925bc3129c4c5cd771cc5cf
nocarryr <matt@nomadic-recording.com>
2018-07-25 09:05:24 -0500

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_long_description():
 
 setup(
     name = "python-dispatch",
-    version = "v0.1.0",
+    version = "v0.1.1",
     author = "Matthew Reid",
     author_email = "matt@nomadic-recording.com",
     description = "Lightweight Event Handling",
