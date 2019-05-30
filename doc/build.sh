311f859b6db7a51137c711eaf12716533fc71449
nocarryr <matt@nomadic-recording.com>
2019-05-30 09:38:43 -0500

    Remove gh-pages and sphinxcontrib-versioning config from docs

--- doc/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-LOCAL_CONF="doc/source/conf.py"
-REPO_DOC_ROOT="doc/source"
-LOCAL_BUILD_DIR="doc/build/html/"
-SPHINX_OPTS="-c doc/source"
-
-sphinx-versioning -l $LOCAL_CONF build $REPO_DOC_ROOT $LOCAL_BUILD_DIR -- $SPHINX_OPTS
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,14 +36,9 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
     'm2r',
 ]
 
-scv_show_banner = True
-scv_banner_greatest_tag = True
-scv_greatest_tag = True
-scv_banner_main_ref = 'v{}'.format(__version__)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
