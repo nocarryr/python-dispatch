d27d90b71378eaac1f9be6bdf683ddb7a800759d
nocarryr <matt@nomadic-recording.com>
2022-04-05 11:22:24 -0500

    Update project metadata with supported Python versions

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     include_package_data=True,
     keywords='event properties dispatch',
     platforms=['any'],
+    python_requires='>=3.6',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -20,13 +21,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
